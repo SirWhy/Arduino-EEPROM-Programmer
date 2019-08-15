1f367fa1c1bcca3619f4122ef02d358bdf8dc090
Mark Anderson <andersm3@tcd.ie>
2019-08-15 14:50:37 +0100

    added serial communication to arduino sketch

--- eeprom_programmer.ino
+++ eeprom_programmer.ino
@@ -5,12 +5,16 @@
 // Quick & dirty EEPROM programmer using shift registers
 // Reads/Writes to ROM
 
-#define WE 13
+#define CE 10
+#define OE 11
+#define WE 12
 #define SHIFT_DATA 2
 #define SHIFT_CLK 3
 #define SHIFT_LATCH 4
-#define D0 5
-#define D7 12
+
+#define READMEM 0xFA
+#define WRITEMEM 0xFB
+#define SUCCESS 0x01
 
 #define ROM_SIZE 8
 
@@ -27,30 +31,107 @@ void setAddr(int addr, bool outputEnable){
 
 // Read one byte at a time
 byte readEEPROM(int addr){
-	for (int pin = D0; pin <= D7; pin++){
-		pinMode(pin, INPUT); // Set pins for reading
-	}
+    DDRD &= 0x00;
 	setAddr(addr, true);
-	byte data = 0;
-	for (int pin = D7; pin >= D0; pin--){
-		data = (data << 1) + digitalRead(pin); // Read each pin individually
-	}
+    digitalWrite(CE, LOW);
+    digitalWrite(OE, LOW);
+	byte data = PIND;
+    digitalWrite(CE, HIGH);
+    digitalWrite(OE, HIGH);
 	return data;
+}
+
+// Read a block of memory
+void readMem(){
+    while (Serial.available < 3) {} // Wait for serial buffer
+    
+    int addr = (Serial.read() << 8); // High byte address
+    addr |= Serial.read(); // Low byte address
+
+    int count = Serial.read();
+    for (int i = 0; i < count; i++){
+        byte data = readEEPROM(addr);
+        Serial.write(data);
+        addr ++;
+    }
+}
+
+// Write to ROM
+void writeMem(){
+    static int ledState = LOW;
+
+    while (Serial.available() < 3) {} // Wait for buffer
+
+    int addr = (Serial.read() << 8); // High byte address
+    addr |= Serial.read(); // Low byte address
+    int count = Serial.read();
+    byte data;
+
+    while (Serial.available() < count) {} // Wait for buffer
+
+    for (int i = 0; i < count; i++){
+        setAddr(addr);
+        DDRD |= 0xFF;
+
+        b = Serial.read();
+        PORTD = b;
+        digitalWrite(WE, LOW);
+        digitalWrite(CE, LOW);
+        digitalWrite(WE, HIGH);
+        digitalWrite(CE, LOW);
+        DDRD &= 0x00;
+        addr++;
+    }
+
+    while (readByte(addr-1) != b) {}
+
+    digitalWrite(13, ledState);
+    ledState = (ledState == HIGH) ? LOW : HIGH;
+
+    Serial.write(SUCCESS);
+}
+
+void error(){
+    for(;;){
+        digitalWrite(13, HIGH);
+        delay(500);
+        digitalWrite(13, LOW);
+        delay(500);
+    }
 }
 
 void setup(){
 	// Set control pins as outputs
+    pinMode(CE, OUTPUT);
+    pinMode(OE, OUTPUT);
 	pinMode(WE, OUTPUT);
 	pinMode(SHIFT_DATA, OUTPUT);
 	pinMode(SHIFT_CLK, OUTPUT);
 	pinMode(SHIFT_LATCH, OUTPUT);
+    
+    digitalWrite(CE, HIGH);
+    digitalWrite(OE, HIGH);
 	digitalWrite(WE, HIGH);
 
-	// Read EEPROM
-	
+	// Initialise Serial
+    Serial.begin(38400);
+
+    // Status LED
+    digitalWrite(13, HIGH);
 }
 
 void loop(){
-	// nothing here yet
-	// need to add serial comms
+    if (Serial.available() > 0){
+        byte in = Serial.read(); // Command byte
+        switch(in){
+            case readMem:
+                readMem();
+                break;
+            case writeMem:
+                writeMem();
+                break;
+            default:
+                error();
+         }
+    }
 }
